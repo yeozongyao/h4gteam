86068d6cc716537558158edfee9b4af6b1bdd34f
yeozongyao <yeozongyao@gmail.com>
2024-02-02 23:51:14 +0800

    test1

--- src/index.js
+++ src/index.js
@@ -10,25 +10,18 @@ import {
 import './style.css'
 import LandingPage from './views/landing-page'
 import NotFound from './views/not-found'
-<<<<<<< HEAD
 import ActivitiesPage from './views/activities-page'
 import CertPage from './views/CertPage'
-=======
 import ProfilesPage from './views/profilespage'
->>>>>>> 0f9334b (test)
 
 const App = () => {
   return (
     <Router>
       <Switch>
-<<<<<<< HEAD
       <Route component={LandingPage} exact path="/" />
         <Route component={ActivitiesPage} exact path="/act" />
         <Route component={CertPage} exact path="/cert" />
-=======
-        <Route component={LandingPage} exact path="/" />
         <Route component={ProfilesPage} exact path="/profiles" />
->>>>>>> 0f9334b (test)
         <Route component={NotFound} path="**" />
 
         <Redirect to="**" />
