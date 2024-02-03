06e8b4827818d9ca31459b81b4775da45b85e50b
shannontyx <shannon18yx@gmail.com>
2024-02-03 14:59:49 +0800

    firebase auth edits

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,4 +22,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth();
+
+export function signup(email, password) {
+    createUserWithEmailAndPassword(auth, email, password);
+}
+
 export default getFirestore();
---
+++ src/views/signin.js
@@ -0,0 +1,11 @@
+import { signup } from "../firebase";
+
+export default function signin() {
+    return (
+        <div id="main">
+            <input placeholder="Email" />
+            <input placeholder="Password" />
+
+        </div>
+    );
+}
