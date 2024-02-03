0c5f1336b0d52c3ea3c34551d686e35d877d166e
vivyanliew <e0969594@u.nus.edu>
2024-02-04 00:40:55 +0800

    can see stuff now

--- src/views/activities-page.js
+++ src/views/activities-page.js
@@ -1,41 +1,41 @@
-import React from 'react'
+import React from "react";
 import { useEffect, useState } from "react";
 import db from "../firebase";
-import PlaceCard from '../components/place-card'
-import '../css/landing-page.css'
-import NavBar from '../components/NavBar'
-import { handleNewEvents, handleEditEvents, handleDeleteEvents, handleQueryDeleteEvents } from  "../UtilEvents"
-import { onSnapshot, collection, addDoc } from 'firebase/firestore';
+import PlaceCard from "../components/place-card";
+import "../css/landing-page.css";
+import NavBar from "../components/NavBar";
+import {
+  handleNewEvents,
+  handleEditEvents,
+  handleDeleteEvents,
+  handleQueryDeleteEvents,
+} from "../UtilEvents";
+import { onSnapshot, collection, addDoc } from "firebase/firestore";
 
 const ActivitiesPage = () => {
-
-    const [event, setEvents] = useState([{name: "Loading..."}]);
-    useEffect(
-      () => onSnapshot(collection(db, "EventsTest"), (snapshot) => 
-      setEvents(snapshot.docs.map((doc) => ({...doc.data(), id: doc.id})))
+  const [event, setEvents] = useState([{ name: "Loading..." }]);
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "EventsTest"), (snapshot) =>
+        setEvents(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
       ),
-      []
-    );
+    []
+  );
 
-    return (
-        <div>
-            <div className='background-image-activites-page'>
-                <NavBar name = "Activities" />
-            </div>
-        <div>
-        
+  return (
+    <div>
+      <div className="background-image-activites-page">
+        <NavBar name="Activities" />
+      </div>
+      <div>
         {event.map((event) => (
-                <div className='landing-page-cards-container' key={event.id}>
-                    <PlaceCard
-                        city={event.name}
-                    ></PlaceCard>
-                </div>
-              ))}
-        </div>
-            
-        </div>
+          <div className="landing-page-cards-container" key={event.id}>
+            <PlaceCard city={event.name}></PlaceCard>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default ActivitiesPage
+export default ActivitiesPage;
--- src/views/admin-add-events.js
+++ src/views/admin-add-events.js
@@ -1,23 +1,21 @@
-import React from 'react'
-import NavBar from '../components/NavBar'
-import '../css/admin-events.css'
+import React from "react";
+import NavBar from "../components/NavBar";
+import "../css/admin-events.css";
 import { Form, Alert, InputGroup, Button, ButtonGroup } from "react-bootstrap";
 import EventDataService from "../UtilEvents";
 import { useEffect, useState } from "react";
 import db from "../firebase";
-import MyForm from '../components/Form'
+import MyForm from "../components/Form";
 
 const AdminAddEvents = () => {
+  return (
+    <div className="adminCert-main-div">
+      <NavBar name="Admin View Events Page" />
+      <div>
+        <MyForm />
+      </div>
+    </div>
+  );
+};
 
-    
-    return (
-        <div className='adminCert-main-div'>
-            <NavBar name = "Admin View Events Page"/>
-            <div>
-            <MyForm/>
-            </div>
-        </div>
-    )
-}
-
-export default AdminAddEvents
+export default AdminAddEvents;
