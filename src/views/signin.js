66108e7b648d90deb7a5708f3ed2dc749d8b2d31
vivyanliew <e0969594@u.nus.edu>
2024-02-04 20:21:37 +0800

    updated signin

--- src/views/signin.js
+++ src/views/signin.js
@@ -1,65 +1,101 @@
 import { useRef, useState } from "react";
 import { signup, useAuth, logout, login } from "../firebase";
-import { useHistory } from 'react-router-dom';
-import NavBar from '../components/NavBar';
-import '../css/signin.css'; // Ensure this is the correct path to your CSS file
+import { useHistory } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import "../css/signin.css"; // Ensure this is the correct path to your CSS file
 
 export default function signin() {
-    const history = useHistory();
-    const [ loading, setLoading ] = useState(false);
-    const currentUser = useAuth();
-    
-    const emailRef = useRef();
-    const passwordRef = useRef();
+  const history = useHistory();
+  const [loading, setLoading] = useState(false);
+  const currentUser = useAuth();
 
-    async function handleSignup() {
-        setLoading(true);
-        try {
-            await signup(emailRef.current.value, passwordRef.current.value);
-            history.push("/profiles");
-        } catch (error) {
-            alert("try again");
-        }
-        setLoading(false);
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  async function handleSignup() {
+    setLoading(true);
+    try {
+      await signup(emailRef.current.value, passwordRef.current.value);
+      history.push("/profiles");
+    } catch (error) {
+      alert("try again");
     }
+    setLoading(false);
+  }
 
-    async function handleLogin() {
-        setLoading(true);
-        try {
-          await login(emailRef.current.value, passwordRef.current.value);
-          // Successful sign up
-        } catch {
-            alert("Error!");
-        }
-        setLoading(false);
+  async function handleLogin() {
+    setLoading(true);
+    try {
+      await login(emailRef.current.value, passwordRef.current.value);
+      // Successful sign up
+    } catch {
+      alert("Error!");
     }
+    setLoading(false);
+  }
 
-    async function handleLogout() {
-        try {
-            await logout();
-        } catch {
-            alert("Error!");
-        }
-        setLoading(false);
+  async function handleLogout() {
+    try {
+      await logout();
+    } catch {
+      alert("Error!");
     }
+    setLoading(false);
+  }
 
-    return (
-        <div id="main" className="signin-container"> 
-            <NavBar name = "Account"/>
+  return (
+    <div id="main" className="signin-container">
+      <NavBar name="Account" />
 
+      <div>Currently logged in as: {currentUser?.email} </div>
 
-            <div>Currently logged in as: { currentUser?.email } </div> 
-            
-            <div id="fields">
-            <input className="input-1" ref={emailRef} placeholder="Email" required/>
-            <input className="input-1" ref={passwordRef} type="password" placeholder="Password" required/>
-
-            </div>
-            <div className="buttonsaccount">
-            <button className="button-40" disabled={ loading || currentUser } onClick={handleSignup} >Sign Up</button>
-            <button className="button-40" disabled={ loading || currentUser } onClick={handleLogin} >Log In</button>
-            <button className="button-40" disabled={ loading || !currentUser } onClick={handleLogout}>Log Out</button>
-            </div>
+      {!currentUser && (
+        <div id="fields">
+          <input
+            className="input-1"
+            ref={emailRef}
+            placeholder="Email"
+            required
+          />
+          <input
+            className="input-1"
+            ref={passwordRef}
+            type="password"
+            placeholder="Password"
+            required
+          />
         </div>
-    );
+      )}
+
+      <div className="buttonsaccount">
+        {!currentUser && (
+          <button
+            className="button-40"
+            disabled={loading}
+            onClick={handleSignup}
+          >
+            Sign Up
+          </button>
+        )}
+        {!currentUser && (
+          <button
+            className="button-40"
+            disabled={loading}
+            onClick={handleLogin}
+          >
+            Log In
+          </button>
+        )}
+        {currentUser && (
+          <button
+            className="button-40"
+            disabled={loading}
+            onClick={handleLogout}
+          >
+            Log Out
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
