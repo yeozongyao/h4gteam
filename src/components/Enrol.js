c62e3b8943963b48742eef28715b9c8aeea01f53
vivyanliew <e0969594@u.nus.edu>
2024-02-07 15:59:09 +0800

    enrolment?

--- src/components/Enrol.js
+++ src/components/Enrol.js
@@ -1,76 +1,96 @@
-import React from 'react'
+import React from "react";
 import { useEffect, useState } from "react";
-import './Enrol.css'; 
-import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
+import "./Enrol.css";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+  setDoc,
+} from "firebase/firestore";
 import { updateDoc, doc } from "firebase/firestore"; // Import Firestore update functions
 import { useAuth } from "../firebase";
 
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 function Enrol(props) {
-    const currentUser = useAuth();
-    const db = getFirestore();
-    
-    const handleEnrolment = async () => {
-        // Check if currentUser is null to avoid errors      
-        if (currentUser && currentUser.email) {
-            const db = getFirestore();
-            const userCollectionRef = collection(db, "User");
-            const userQuery = query(userCollectionRef, where("email", "==", currentUser.email));
-        
-            try {
-                const userSnapshot = await getDocs(userQuery);
-                if (!userSnapshot.empty) {
-                    const userId = userSnapshot.docs[0].id;
-                    
-                    // Update the user document to add the event ID to the enrolledEvents array
-                    await updateDoc(doc(db, "User", userId), {
-                        enrolledEvents: [...(userSnapshot.docs[0].data().enrolledEvents || []), props.eventId]
-                    });
-                    console.log("can add events to user");
+  const currentUser = useAuth();
+  const db = getFirestore();
 
-                    // Add the user ID to the event document in the Events collection
-                    await updateDoc(doc(db, "EventsTest", props.eventId), {
-                        enrolledUsers: [...(userSnapshot.docs[0].data().enrolledUsers || []), userId]
-                    });
-                    console.log("can add user to event");
+  const handleEnrolment = async () => {
+    // Check if currentUser is null to avoid errors
+    if (currentUser && currentUser.email) {
+      const db = getFirestore();
+      const userCollectionRef = collection(db, "User");
+      const userQuery = query(
+        userCollectionRef,
+        where("email", "==", currentUser.email)
+      );
 
-                    // Combine MLData using Event Description
-                    const updatedMLData = userData.MLData ? `${userData.MLData} ${eventDescription}` : eventDescription;
+      try {
+        const userSnapshot = await getDocs(userQuery);
+        if (!userSnapshot.empty) {
+          const userId = userSnapshot.docs[0].id;
 
-                    // Update user profile ML Data with the enrolled event description
-                    await updateDoc(
-                      userDocRef,
-                      { MLData: updatedMLData },
-                    );
-                    
-                    // Close the popup after enrolment
-                    props.setTrigger(false);
+          // Update the user document to add the event ID to the enrolledEvents array
+          await updateDoc(doc(db, "User", userId), {
+            enrolledEvents: [
+              ...(userSnapshot.docs[0].data().enrolledEvents || []),
+              props.eventId,
+            ],
+          });
+          console.log("can add events to user");
 
-                    alert("Successfully addded");
-                }
-            } catch (error) {
-                console.error("Error updating", error);
-                }
-            } 
-        
-    };
+          // Add the user ID to the event document in the Events collection
+          await updateDoc(doc(db, "EventsTest", props.eventId), {
+            enrolledUsers: [
+              ...(userSnapshot.docs[0].data().enrolledUsers || []),
+              userId,
+            ],
+          });
+          console.log("can add user to event");
 
-    return (props.trigger) ? (
-        <div className='popup'>
-            <div className='popup-inner'>
-                <div className='button-container'>
-                    {}
-                    <button onClick={handleEnrolment}>
-                        Confirm Enrolment
-                    </button>
-                    <button onClick={() => props.setTrigger(false)}>
-                        X
-                    </button>
-                    
-                </div>
-            </div>
+          // Combine MLData using Event Description
+          // const updatedMLData = userData.MLData ? `${userData.MLData} ${eventDescription}` : eventDescription;
+
+          // Update user profile ML Data with the enrolled event description
+          // await updateDoc(
+          //   userDocRef,
+          //   { MLData: updatedMLData },
+          // );
+
+          // Close the popup after enrolment
+          props.setTrigger(false);
+
+          const eventDocRef = doc(db, "EventsTest", props.eventId);
+          const eventPax = props.eventPax;
+          await setDoc(
+            eventDocRef,
+            { eventPax: eventPax - 1 },
+            { merge: true }
+          );
+
+          alert("Successfully addded");
+        }
+      } catch (error) {
+        console.error("Error updating", error);
+      }
+    }
+  };
+
+  return props.trigger ? (
+    <div className="popup">
+      <div className="popup-inner">
+        <div className="button-container">
+          {}
+          <button onClick={handleEnrolment}>Confirm Enrolment</button>
+          <button onClick={() => props.setTrigger(false)}>X</button>
         </div>
-    ): "";
+      </div>
+    </div>
+  ) : (
+    ""
+  );
 }
-export default Enrol
+export default Enrol;
--- src/components/event-card.js
+++ src/components/event-card.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-import OutlineButton from './outline-button'
-import './event-card.css'
-import Enrol from './Enrol.js'
-import { useState } from 'react';
+import OutlineButton from "./outline-button";
+import "./event-card.css";
+import Enrol from "./Enrol.js";
+import { useState } from "react";
 import { useAuth } from "../firebase";
 
 const EventCard = (props) => {
@@ -21,30 +21,38 @@ const EventCard = (props) => {
       <div className="place-card-container1">
         <span className="place-card-text">{props.city}</span>
         <span className="place-card-text1">{props.description}</span>
-        <span className="place-card-text1">Looking for: <span className="place-card-text">{props.eventPax}</span> volunteers</span>
-        <button className='button-40'onClick={() => setButtonPopup(true)}>Enrol for Event Now</button>
-          <Enrol trigger={buttonPopup} setTrigger={setButtonPopup} eventId={props.eventId} />
-            
-            
+        <span className="place-card-text1">
+          Looking for: <span className="place-card-text">{props.eventPax}</span>{" "}
+          volunteers
+        </span>
+        <button className="button-40" onClick={() => setButtonPopup(true)}>
+          Enrol for Event Now
+        </button>
+        <Enrol
+          trigger={buttonPopup}
+          setTrigger={setButtonPopup}
+          eventId={props.eventId}
+          eventPax={props.eventPax}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
 EventCard.defaultProps = {
   image:
-    'https://images.unsplash.com/photo-1514924013411-cbf25faa35bb?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1000',
-  imageAlt: 'image',
-  city: 'City Name',
+    "https://images.unsplash.com/photo-1514924013411-cbf25faa35bb?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1000",
+  imageAlt: "image",
+  city: "City Name",
   description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
-}
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.",
+};
 
 EventCard.propTypes = {
   image: PropTypes.string,
   imageAlt: PropTypes.string,
   city: PropTypes.string,
   description: PropTypes.string,
-}
+};
 
-export default EventCard
+export default EventCard;
