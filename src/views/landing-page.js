2b0843441d1c6a85f389067dae3d20ad2185c782
shannontyx <shannon18yx@gmail.com>
2024-02-09 02:48:32 +0800

    Home page

--- src/views/landing-page.js
+++ src/views/landing-page.js
@@ -69,10 +69,16 @@ const LandingPage = () => {
           <div className="blueheart">
             <img src={blueheart} alt="blueheart" />
           </div>
+
+          </div>
+       
+       
+        <div className="parting-container">
+          <div className="recommend2">
+            <h2>Here is how you can help! Events recommended for you!</h2>
+          </div>
         </div>
-        <div className="partingContainer">
-          Here is how you can help! Events recommended for you!!
-        </div>
+
         <div className="similar-events-container">
         <EventSimilarity/>
         </div>
@@ -82,51 +88,6 @@ const LandingPage = () => {
 
      
       <div className="landing-page-top-container">
-        <div className="landing-page-hero">
-          <div className="landing-page-content-container">
-            <h1 className="Heading landing-page-text09">Just be good</h1>
-
-            <button className="newbutton" onClick={handleNew}>
-              New
-            </button>
-            <button className="querydeletebutton" onClick={handleQueryDelete}>
-              Query Delete
-            </button>
-            <ul>
-              <p>DATABASE ONES</p>
-              {colors.map((color) => (
-                <li key={color.id}>
-                  <button
-                    className="editbutton"
-                    onClick={() => handleEdit(color.id)}
-                  >
-                    edit
-                  </button>
-                  <button
-                    className="deletebutton"
-                    onClick={() => handleDelete(color.id)}
-                  >
-                    delete
-                  </button>
-                  <Dot color={color.value} /> {color.name}
-                </li>
-              ))}
-
-              <p>HARDCODED ONESSS</p>
-
-              <li>
-                <a href="#">edit</a> <Dot color="#f00" /> Red
-              </li>
-              <li>
-                <a href="#">edit</a> <Dot color="#0f0" /> Green
-              </li>
-              <li>
-                <a href="#">edit</a> <Dot color="#00f" /> Blue
-              </li>
-            </ul>
-            <SolidButton button="Explore homes"></SolidButton>
-          </div>
-        </div>
       </div>
       <div id="main-section" className="landing-page-main">
       </div>
